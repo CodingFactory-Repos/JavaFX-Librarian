6d643da9fbfb9092abd75a08b444f3af2409f0ca
Gregory C <91255543+SetsunaGg@users.noreply.github.com>
2021-12-01 16:48:15 +0100

    Skeleton Scene Builder

---
+++ src/main/java/me/loule/librarian/controller/MonApplicationController.java
@@ -0,0 +1,34 @@
+package me.loule.librarian.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class MonApplicationController {
+
+    @FXML
+    private Button Supprimer;
+
+    @FXML
+    private Button Valider;
+
+    @FXML
+    private TextField txtAuteur;
+
+    @FXML
+    private TextField txtColonne;
+
+    @FXML
+    private TextField txtParution;
+
+    @FXML
+    private TextField txtRangee;
+
+    @FXML
+    private TextArea txtResume;
+
+    @FXML
+    private TextField txtTitre;
+
+}
