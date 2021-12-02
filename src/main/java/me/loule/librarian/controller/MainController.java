ae407c79aa4ffe07e34fcc1da3ee89a34e885464
Antony <91181127+Getsugo@users.noreply.github.com>
2021-12-02 10:56:44 +0100

    import fx:id

--- src/main/java/me/loule/librarian/controller/MainController.java
+++ src/main/java/me/loule/librarian/controller/MainController.java
@@ -1,14 +1,73 @@
 package me.loule.librarian.controller;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import me.loule.librarian.module.TableViews;
 
-public class MainController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+public class MainController implements Initializable {
+
+        @FXML
+        private TableColumn<TableViews, String> ColumnAuteur;
+
+        @FXML
+        private TableColumn<TableViews, String> ColumnColonne;
+
+        @FXML
+        private TableColumn<TableViews, String> ColumnParution;
+
+        @FXML
+        private TableColumn<TableViews, String> ColumnRangee;
+
+        @FXML
+        private TableColumn<TableViews, String> ColumnResume;
+
+        @FXML
+        private TableColumn<TableViews, String> ColumnTitre;
+
+        @FXML
+        private MenuItem Fichier;
+
+        @FXML
+        private MenuItem Quitter;
+
+        @FXML
+        private Button Supprimer;
+
+        @FXML
+        private TableView<TableViews> TableTableau;
+
+        @FXML
+        private Button Valider;
+
+        @FXML
+        private TextField txtAuteur;
+
+        @FXML
+        private TextField txtColonne;
+
+        @FXML
+        private TextField txtParution;
+
+        @FXML
+        private TextField txtRangee;
+
+        @FXML
+        private TextArea txtResume;
+
+        @FXML
+        private TextField txtTitre;
+
+
+
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 }
---
+++ src/main/java/me/loule/librarian/module/TableViews.java
@@ -0,0 +1,5 @@
+package me.loule.librarian.module;
+
+public class TableViews {
+
+}
