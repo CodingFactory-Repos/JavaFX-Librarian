4baada413873e4df6a2be95404916987f6c3f602
Louis <35641311+Loule95450@users.noreply.github.com>
2021-12-02 22:36:58 +0100

    🔁 - Revert old commit

--- src/main/java/me/loule/librarian/HelloApplication.java
+++ src/main/java/me/loule/librarian/HelloApplication.java
@@ -7,12 +7,18 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class Main extends Application {
+public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+<<<<<<< HEAD:src/main/java/me/loule/librarian/Main.java
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Main.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Biblio");
+=======
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+>>>>>>> parent of e1efcd7 (:pencil2: - Rename HelloApplication to Main):src/main/java/me/loule/librarian/HelloApplication.java
         stage.setScene(scene);
         stage.show();
     }
